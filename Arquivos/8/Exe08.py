a2de3dc31a2370881e342782d6ea6a9341093abd
Matheus Koyama <koyamaCRS@gmail.com>
2021-11-06 15:25:32 -0300

    innitial commit

---
+++ Arquivos/8/Exe08.py
@@ -0,0 +1,6 @@
+# Exercício 8 - Considere o código abaixo. Obtenha o mesmo resultado usando o pacote time. 
+import datetime
+print (datetime.datetime.now().strftime("%d/%m/%Y %H:%M"))
+
+import time
+print(time.strftime("%d/%m/%Y %H:%M"))
